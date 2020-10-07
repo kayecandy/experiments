2fcee464d9746e9ca4b57685c317da3c51a688ad
Candice Canoso <kaye_candy14@yahoo.com>
2020-10-07 20:55:45 +0800

    [sankey] - Added transitions

--- sankey/cndce-sankey.js
+++ sankey/cndce-sankey.js
@@ -1,7 +1,8 @@
 
 (()=>{
+	console.log(document.body.clientWidth)
 
-	const WIDTH = 2000;
+	const WIDTH = Math.max(1300, document.body.clientWidth - 40);
 	const HEIGHT = 3000;
 	const MARGIN = {
 		TOP: 10,
@@ -15,15 +16,25 @@
 
 	var OPACITY_DISABLED = 0.3;
 
+	var NODE_TRANSITION_LIMIT = 400;
+	var TRANSITION_DURATION = 500;
+
 
 	var cndceSankey;
 	var canvas;
+	var ctx;
 	var svg;
 	var sankey;
 	var data;
 
+	var detachedLinks;
+
+
 	var persistActive = false;
 	var activeGraph;
+	var activeNode;
+
+	var drawTimer;
 	
 
 	(async function init(){
@@ -31,11 +42,7 @@
 		await initData();
 		initSankey();
 		initD3();
-		// drawCanvas();
-
-		d3.timer(()=>{
-			drawCanvas()
-		})
+		drawCanvas();
 	})();
 
 
@@ -49,10 +56,7 @@
 		sankey = d3.sankey()
 		.nodeId(d => d.id)
 		.nodeSort(null)
-		.nodeAlign((node, n) => {
-			// console.log(node, node.layer, d3.sankeyJustify(node, n));
-			return node.layer ? node.layer : d3.sankeyJustify(node, n);
-		})
+		.nodeAlign(customNodeAlign)
 		.extent([[MARGIN.LEFT, MARGIN.TOP],[WIDTH + MARGIN.LEFT, HEIGHT + MARGIN.TOP]]);
 
 
@@ -76,44 +80,49 @@
 			// .attr('style', `margin: ${MARGIN.TOP}px ${MARGIN.RIGHT}px ${MARGIN.BOTTOM}px 0`);
 
 
+		ctx = canvas.node().getContext('2d')
+
+
 		svg.append('g')
 			.attr('class', 'cndce-sankey__nodes')
 
 		svg.append('g')
 			.attr('class', 'cndce-sankey__labels')
 
+		detachedLinks = d3.select(document.createElement('g'))
+			.attr('class', 'cndce-sankey__links')
+
 		updateD3();
 	};
 
 
 	function updateD3(){
-  		// svg.append("g")
-    //   		.style("font", "10px sans-serif")
-		  //   .selectAll("text")
-		  //   .data(nodes)
-		  //   .join("text")
-		  //     .attr("x", d => d.x0 < WIDTH / 2 ? d.x1 + 6 : d.x0 - 6)
-		  //     .attr("y", d => (d.y1 + d.y0) / 2)
-		  //     .attr("dy", "0.35em")
-		  //     .attr("text-anchor", d => d.x0 < WIDTH / 2 ? "start" : "end")
-		  //     .text(d => d.name)
-		  //   .append("tspan")
-		  //     .attr("fill-opacity", 0.7)
-		  //     .text(d => ` ${d.value.toLocaleString()}`)
 
 		const {nodes, links} = sankey();
+
+		console.log(nodes);
 
 		const svgLabels = svg.select('.cndce-sankey__labels')
 			.selectAll('.cndce-sankey__label')
 			.data(nodes, d => d.id)
 
+		const textAlign = d => {
+			if(d.x1 < WIDTH / 2){
+				d.textAnchor = 'start';
+				return d.x1 + 6;
+			}
+
+			d.textAnchor = 'end';
+			return d.x0 - 6;
+		}
+
 		svgLabels.enter()
 			.append("text")
 				.attr('class', 'cndce-sankey__label')
-				.attr("x", d => d.x0 < WIDTH / 2 ? d.x1 + 6 : d.x0 - 6)
+				.attr("x", textAlign)
 			    .attr("y", d => (d.y1 + d.y0) / 2)
 			    .attr("dy", "0.35em")
-			    .attr("text-anchor", d => d.x0 < WIDTH / 2 ? "start" : "end")
+			    .attr("text-anchor", d => d.textAnchor)
 		    	.attr("opacity", d => persistActive && !d.active ? 0 : 1)
 			    .text(d => d.name)
 
@@ -122,11 +131,15 @@
 			    .text(d => ` ${d.value.toLocaleString()}`)
 
 		svgLabels
-			// .transition()
-			// .duration(1500)
-			.attr("x", d => d.x0 < WIDTH / 2 ? d.x1 + 6 : d.x0 - 6)
+			.transition()
+			.duration(nodes.length < NODE_TRANSITION_LIMIT ? TRANSITION_DURATION : 0)
+			.attr("x", textAlign)
 		    .attr("y", d => (d.y1 + d.y0) / 2)
+		    // .attr("text-anchor", d => d.textAnchor)
 	    	.attr("opacity", d => persistActive && !d.active ? 0 : 1)
+
+	    svgLabels
+	    	.attr('text-anchor', d => d.textAnchor)
 
 
 		svgLabels.exit().remove();
@@ -154,11 +167,10 @@
 		    .append("title")
 		      	.text(d => `${d.name}\n${d.value.toLocaleString()}`);
 
-		// console.log(svgNodes.selectAll)
 
 		svgNodes
-			// .transition()
-			// .duration(500)
+			.transition()
+			.duration(nodes.length < NODE_TRANSITION_LIMIT ? TRANSITION_DURATION : 0)
 			.attr("x", d => d.x0 + 1)
 	    	.attr("y", d => d.y0)
 	    	.attr("height", d => Math.max(d.y1 - d.y0, 3))
@@ -166,46 +178,100 @@
 	    	.attr("opacity", d => persistActive && !d.active ? 0 : 1)
 
 
-
 		svgNodes.exit().remove();
+
+
+
+		const svgLinks = detachedLinks
+			.selectAll('.cndce-sankey__link')
+			.data(links, d => d.source.id + ':' + d.target.id)
+
+
+		svgLinks.enter()
+			.append('path')
+			.attr('class', 'cndce-sankey__link')
+			.attr("d", d3.sankeyLinkHorizontal())
+		    .attr("stroke-width", d => Math.max(1, d.width))
+
+
+
+		svgLinks
+			.transition()
+			.duration(nodes.length < NODE_TRANSITION_LIMIT ? TRANSITION_DURATION : 0)
+		    .attr("stroke-width", d => Math.max(1, d.width))
+		    .attrTween('d', function(d){
+
+		    	if(nodes.length >= NODE_TRANSITION_LIMIT){
+		    		return;
+		    	}
+
+		    	const prevNode = d3.select(this);
+
+		    	const interpolateWidth = d3.interpolateNumber(
+		    		prevNode.attr('stroke-width'),
+		    		Math.max(1, d.width)
+		    	);
+
+		    	const interpolatePath = d3.interpolatePath(
+		    		prevNode.attr('d'),
+		    		d3.sankeyLinkHorizontal()(d)
+		    	)
+
+    			clearCanvas();
+
+
+		    	return (t) => {
+		    		if(d == links[0]){
+		    			clearCanvas();
+		    		}
+
+		    		drawLink(d, interpolatePath(t), interpolateWidth(t));
+
+		    	}
+		    })
+
+		svgLinks.exit().remove();
 	}
 
+	const drawLink = (d, path='', width='') => {
+		const _canvas = canvas.node();
 
-	function drawCanvas(){
-		const {links} = sankey();
+		const p = new Path2D(path ? path : d3.sankeyLinkHorizontal()(d))
+		ctx.lineWidth = Math.max(1, width ? width : d.width);
 
-		console.log(links);
-
-		_canvas = canvas.node();
-		ctx = _canvas.getContext('2d')
-		ctx.clearRect(0, 0, _canvas.width, _canvas.height);
-
-		const drawLink = (d) => {
-			const p = new Path2D(d3.sankeyLinkHorizontal()(d))
-			ctx.lineWidth = Math.max(1, d.width);
-
-
-			if(!d.active){
-				ctx.strokeStyle = d3.color(d.color) || COLOR_LINKS;
-			}else{
-				ctx.strokeStyle = COLOR_LINK_ACTIVE;
-			}
-
-			ctx.stroke(p);
-
+		if(!d.active){
+			ctx.strokeStyle = d3.color(d.color) || COLOR_LINKS;
+		}else{
+			ctx.strokeStyle = COLOR_LINK_ACTIVE;
 		}
 
+		ctx.stroke(p);
+
+	}
+
+	function clearCanvas(){
+		const _canvas = canvas.node();
+
+		ctx.clearRect(0, 0, _canvas.width, _canvas.height);
+	}
+
+	function drawCanvas(){
 
 
-		if(!persistActive)
-			links.forEach((d, i)=>{
-				drawLink(d);
-			})
+		const {links} = sankey();
 
-		activeGraph.links.forEach((d)=>{
+
+
+		clearCanvas();
+
+
+		links.forEach((d, i)=>{
 			drawLink(d);
 		})
 
+		activeGraph.links.forEach((d, i)=>{
+			drawLink(d);
+		})
 
 
 		window.sankey = sankey;
@@ -213,14 +279,11 @@
 		window.ctx = ctx;
 	}
 
-	// function activateNodes(){
-	// 	const nodeSelector = activeGraph.nodes.map( 
-	// 		node => `.cndce-sankey__node[data-node-id=${node.id}]`
-	// 	).join(',')
 
-	// 	d3.selectAll(nodeSelector)
-
-	// }
+	function customNodeAlign(node, n) {
+		// console.log(node, node.layer, d3.sankeyJustify(node, n));
+		return node.custLayer ? node.custLayer : d3.sankeyJustify(node, n);
+	}
 
 
 	function traverseNode(node, keyNodeNext = 'source', keyLinkNext = 'targetLinks'){
@@ -229,13 +292,23 @@
 		var links = [];
 
 
+
 		node[keyLinkNext].forEach((d, i)=>{
 			links.push(d);
 
 			const sourceGraph = traverseNode(d[keyNodeNext], keyNodeNext, keyLinkNext);
 
-			nodes = nodes.concat(sourceGraph.nodes);
-			links = links.concat(sourceGraph.links);
+			sourceGraph.nodes.forEach(n=>{
+				if(nodes.indexOf(n) == -1){
+					nodes.push(n)
+				}
+			})
+
+			sourceGraph.links.forEach(l=>{
+				if(links.indexOf(l) == -1){
+					links.push(l)
+				}
+			})
 		})
 
 		return {nodes, links};
@@ -308,47 +381,61 @@
 	function handleNodeClick(e, d){
 		persistActive = !persistActive;
 
-		// if(persistActive){
-			// sankey
-			// 	.nodes(activeGraph.nodes.map(d3NodeToRawData))
-			// 	.links(activeGraph.links.map(d3LinkToRawData))
-			// 	.extent([[MARGIN.LEFT, MARGIN.TOP],[WIDTH + MARGIN.LEFT, HEIGHT/2 + MARGIN.TOP]]);
+		if(persistActive){
 
-			// svg.transition()
-			// 	.duration(1000)
-			// 	.attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-			// 	.attr('height', HEIGHT/2 + MARGIN.TOP + MARGIN.BOTTOM);
+			const height = Math.max(document.documentElement.clientHeight - MARGIN.TOP - MARGIN.BOTTOM, HEIGHT * activeGraph.nodes.length * 0.0015);
 
-			// canvas
-			// 	.attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-			// 	.attr('height', HEIGHT/2 + MARGIN.TOP + MARGIN.BOTTOM);
-		// }else{
-		// 	sankey
-		// 		.nodes(data.nodes)
-		// 		.links(data.links)
-		// }
+			sankey
+				.nodes(activeGraph.nodes)
+				.links(activeGraph.links)
+				.nodeAlign(d3.sankeyJustify)
+				.extent([[MARGIN.LEFT, MARGIN.TOP],[WIDTH + MARGIN.LEFT, height + MARGIN.TOP]]);
 
-		if(!persistActive){
+			svg
+			.attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+			.attr('height', height + MARGIN.TOP + MARGIN.BOTTOM);
+
+			canvas
+			.attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+			.attr('height', height + MARGIN.TOP + MARGIN.BOTTOM);
+
+
+
+
+			activeGraph.links.forEach(l=>{
+				l.active = false;
+			})
+
+		}else{
+
+			sankey
+				.nodes(data.nodes)
+				.links(data.links)
+				.nodeAlign(customNodeAlign)
+				.extent([[MARGIN.LEFT, MARGIN.TOP],[WIDTH + MARGIN.LEFT, HEIGHT + MARGIN.TOP]]);
+
+			svg
+			.attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+			.attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
+
+			canvas
+			.attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+			.attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
+
+
 			resetActiveGraph();
+
 		}
 
-		try{
-			updateD3()
-			// drawCanvas();
-		}catch(err){
-			d.disabled = true;
+		updateD3();
+		drawCanvas();
 
-			alert('Sorry. Data in this node causes and error and needs to be cleaned.')
-
-			resetActiveGraph();
-			handleNodeClick(e, d);
-		}
 
 	}
 
 	function handleNodeMouseOver(e, d){
-		if(persistActive)
-			return;
+		// if(persistActive)
+		// 	return;
 
 		activeNode = d;		
 		activeGraph = getActiveGraph(d);
@@ -362,19 +449,25 @@
 		})
 
 
-		// drawCanvas();
+		drawCanvas();
 		window.activeGraph = activeGraph;
 		window.updateD3 = updateD3;
 	}
 
 	function handleNodeMouseOut(e, d){
-		if(persistActive)
+		if(persistActive){
+			activeGraph.links.forEach(l=>{
+				l.active = false;
+			})
+
+			drawCanvas();
 			return;
+		}
 
 
 		activeNode = undefined;
 		resetActiveGraph();
-		// drawCanvas();
+		drawCanvas();
 		
 	}
 
