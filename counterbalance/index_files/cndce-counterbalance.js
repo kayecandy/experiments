055d2882a22aa86ccce0c6f0f712253694742ee5
Candice Canoso <kaye_candy14@yahoo.com>
2021-03-19 13:55:13 +0800

    [counterbalance] - init

---
+++ counterbalance/index_files/cndce-counterbalance.js
@@ -0,0 +1,70 @@
+(() => {
+
+	var player = jwplayer("myElement").setup({
+	    playlist: "./testrss.json",
+	   	allowfullscreen: "true",
+		repeat: "list",
+		preload: "auto",
+		modes: [{ type: 'html5' }]
+	});
+
+	var playerContainer;
+	var sliderContainer;
+
+	var domPreviousRail;
+	var domNextRail;
+
+
+	// Initialize DOM elements
+	player.on('ready', function(){
+		playerContainer = document.getElementById('myElement');
+		sliderContainer = playerContainer.querySelector('.jw-slider-container');
+
+		// Add additional rail elements
+		domPreviousRail = document.createElement('div');
+		domPreviousRail.classList.add('cndce-prev-rail');
+
+		domNextRail = document.createElement('div');
+		domNextRail.classList.add('cndce-next-rail');
+
+		sliderContainer.prepend(domNextRail);
+		sliderContainer.prepend(domPreviousRail)
+	})
+
+	player.on('meta', function(meta){
+		var currPlaylist = player.getPlaylistItem();
+
+		
+		domPreviousRail.style.width = (currPlaylist.sources[0].starttime /  meta.seekRange.end) * 100 + '%';
+
+
+		
+		domNextRail.style.width = (1 - (currPlaylist.end / meta.seekRange.end)) * 100 + '%';
+
+
+	})
+
+
+
+	player.on('time', function({position}){
+		var currPlaylist = player.getPlaylistItem();
+		var iPlaylist = player.getPlaylistIndex();
+
+
+		if(position > currPlaylist.end){
+			player.next();
+		}else if(position < currPlaylist.sources[0].starttime){
+			player.playlistItem(iPlaylist - 1);
+		}
+	})
+
+	player.on('playlistItem', function({index, item}){
+		console.log('test');
+
+		// Ensure playlist always follows starttime
+		player.seek(item.sources[0].starttime);
+
+	})
+
+})();
+
