78a84b5199047f23f1e5c44005ace9ca37f41f59
Candice Canoso <kaye_candy14@yahoo.com>
2021-08-10 04:53:31 +0800

    [boxinggloves] - Transfer to own repo boxxerworld

--- boxinggloves/assets/themes/boxxerworld/components/controls-top.php
+++
@@ -1,8 +0,0 @@
-<div class="cndce-controls-top">
-    <?php foreach(CONFIG['controls-top'] as $topControl): ?>
-        <div class="cndce-control-top">
-            <?php include( __DIR__ . '/../' . $topControl['icon']) ?>
-            <div class="cndce-control-top--title"><?= $topControl['title'] ?></div>
-        </div>
-    <?php endforeach; ?>
-</div>
